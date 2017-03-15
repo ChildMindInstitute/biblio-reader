db73b53babba23aa390976115b2ef06edff3b4b4
Michael Fleischmann <michael.fleischmann@CMI-RSCH-IM019.local>
2017-03-15 16:00:09 -0400

    Starting with some basic functions in this class

--- scholar_reader.py
+++ scholar_reader.py
@@ -3,12 +3,28 @@ import pandas as pd
 with open('FCP_2015.csv', 'r') as f:
     data = pd.read_csv(f)
 
-journal = data['Journal'].dropna().apply(lambda x: str(x).casefold())
 
-year = data['Year'].dropna()
+def countstats(series, path=None, row_limit=None):
+    stat = series.dropna().apply(lambda x: str(x).casefold()).value_counts()
+    if row_limit:
+        stat = stat.head(row_limit)
+    if path:
+        stat.to_csv(path=path, header=True)
+    else:
+        print(stat)
 
-print(journal.value_counts().head(20))
 
-journals = data.groupby('Journal').groups
+def citations_per_year(sort=False):
+    data['CPY'] = data['Citations'] / (2018 - data['Year'])
+    if sort:
+        data.sort_values('CPY', inplace=True, ascending=False)
+        data.reset_index(drop=True, inplace=True)
+
+
+def count_tuples():
+    journal_and_year = pd.Series(list(zip(data['Journal'].dropna(), data['Year'].dropna())))
+    countstats(journal_and_year)
+count_tuples()
+
 
 
