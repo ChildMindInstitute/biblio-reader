df13d971ac6474799f3659d6802c8c74452a29c9
mfleisch50 <mfleisch50@gmail.com>
2017-05-15 10:49:19 -0400

    Assignment generator clean-up

--- biblio_reader/list_gen.py
+++ biblio_reader/list_gen.py
@@ -1,4 +1,4 @@
-import random, os, csv, ast, collections
+import random, os, csv, ast, collections, math
 import pandas as pd
 
 with open('../inputs/FCP_DATA.csv', 'r') as f, open('../outputs/paragraphs.txt', 'r') as p, \
@@ -29,17 +29,15 @@ def checker_directory(directory):
 double_checked = [(key, check) for key, check in checker_directory('../inputs/Article_Checks').items() if len(check) == 2]
 conflicts = [(key, check) for key, check in sorted(double_checked) if check[0][1] != check[1][1]]
 
-#print(len(double_checked))
-#print(len(conflicts), *conflicts, sep='\n')
 
 assign = list(data['i'])
 assign_copy = list(assign)
 
 
 class Member(object):
-    def __init__(self, name):
+    def __init__(self, name, path):
         self.name = name
-        self.path = '../outputs/Assignments/' + name + '.txt'
+        self.path = '/'.join([path, name + '.txt'])
         if os.path.exists(self.path):
             file = open(self.path)
             for i, line in enumerate(file):
@@ -51,7 +49,7 @@ class Member(object):
         self.written = list(self.articles)
 
     def __str__(self):
-        return self.name + '\n\n\n' + ','.join([str(article) for article in self.articles]) + '\n\n\n' + \
+        return self.name + '\n\n\n' + ','.join([str(article) for article in sorted(self.articles)]) + '\n\n\n' + \
            '\n\n'.join(['ARTICLE NO ' + str(key) + ': ' + str(data.iloc[key]['Title']) +
                         '\n' + str(data.iloc[key]['Authors']) + '\nPublication Type: ' +
                         data.iloc[key]['Journal Category'] + '\n\n' + '\n\n'.join(paragraph)
@@ -60,7 +58,7 @@ class Member(object):
            '\n\n'.join(['ARTICLE NO ' + str(article) + ': ' + str(data.iloc[article]['Title']) +
                         '\n' + str(data.iloc[article]['Authors']) + '\n' + str(data.iloc[article]['URL']) +
                         '\nPublication Type: ' + data.iloc[article]['Journal Category'] for
-                        article in self.articles if article in list(bad_data['i'])])
+                        article in self.articles if article in sorted(list(bad_data['i']))])
 
     def assign(self, assign, length):
         while length > 0 and not set(assign).issubset(set(self.articles)):
@@ -71,16 +69,16 @@ class Member(object):
                 length -= 1
 
 
-
-
-
 class Assignment(object):
-    def __init__(self, members):
-        self.members = [Member(member) for member in members]
-
+    def __init__(self, members=None, dir='../outputs/Assignments'):
+        if members is None:
+            self.members = [Member(member.replace('.txt', ''), dir) for member in os.listdir(dir) if
+                            '.txt' in member]
+        else:
+            self.members = [Member(member, dir) for member in members]
 
     def __getitem__(self, item):
-        if item in range(0, len(self.members) - 1):
+        if item in range(0, len(self.members)):
             return self.members[item]
         for member in self.members:
             if item == member.name:
@@ -104,19 +102,19 @@ class Assignment(object):
                 with open(member.path, 'w') as f:
                     f.write(str(member))
 
-    def reassign(self):
+    def double(self):
         articles = []
         for member in self.members:
             articles += member.articles
         assignment = [item for item, count in collections.Counter(articles).items() if count == 1]
-        member_length = (len(articles) + len(assignment)) / len(self.members)
+        member_length = math.ceil((len(articles) + len(assignment)) / len(self.members))
         revert = None
         while len(assignment) > 0:
             for member in self.members:
                 if revert:
                      while revert > 0:
                          reverted = random.choice(member.articles)
-                         if reverted not in assignment:
+                         if reverted not in assignment and reverted not in member.written:
                              assignment.append(reverted)
                              member.articles.remove(reverted)
                              revert -= 1
@@ -128,26 +126,26 @@ class Assignment(object):
                     if len(member.articles) != member_length:
                         revert = len(assignment)
 
-    def test(self, test_type=None):
-        if test_type == 1 or test_type is None:
+    def test(self, test_type=list([1, 2, 3])):
+        if 1 in test_type:
             articles = []
             for member in self.members:
                 articles += member.articles
             print('Duplicate items:',
                   len([item for item, count in collections.Counter(articles).items() if count == 2]))
-        if test_type == 2 or test_type is None:
+        if 2 in test_type:
             for member in self.members:
                 print(member.name.capitalize() + "'s Length:", len(member.articles), '| Duplicates:',
-                      len([item for item, count in collections.Counter(member.articles).items() if count == 2]))
+                      len([item for item, count in collections.Counter(member.articles).items() if count > 1]))
+        if 3 in test_type:
+            for member in self.members:
+                print(member.name.capitalize() + "'s New Articles:",
+                      len([article for article in member.articles if article not in member.written]), '| Old:',
+                      len(member.written))
 
 
+assignment = Assignment(members=['miaslkdjf', 'asdjfiaskdl', 'dfsdfsdfsasdfasdcdasd', 'ewrfs', 'dsf', 'a', 'werwerwerwer', 'wer', 'werr'])
 
-
-
-assignment = Assignment(['anirudh', 'bonhwang', 'michael', 'helen', 'jon'])
-
-assignment.reassign()
-assignment.reassign()
-assignment.reassign()
-assignment.test()
-
+assignment.assign(assign)
+assignment.double()
+assignment.test([1, 2])
