8304acfdc82f6958ec7fb5ed3010d430cb9adbf3
mfleisch50 <mfleisch50@gmail.com>
2017-05-26 14:07:27 -0400

    Data, new author text utils, and more!

--- table/data_mg.py
+++ table/data_mg.py
@@ -3,6 +3,7 @@ import collections
 
 data = mg.get_data()
 
-table_data = data[['Title', 'Authors', 'Journal', 'Year', 'CPY', 'URL']].copy()
+data = data[data['Data Use'] != 'I']
+table_data = data[['Title', 'Authors', 'Journal', 'Year', 'CPY', 'URL']]
 
 table_data.to_csv(path_or_buf='Table_data.csv', index=False)
