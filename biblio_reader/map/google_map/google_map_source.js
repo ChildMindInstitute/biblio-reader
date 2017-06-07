9ecb8df6c3d98e508770b77bb544ee2d7af91bd2
mfleisch50 <mfleisch50@gmail.com>
2017-06-07 16:01:39 -0400

    Google map success! :)

--- biblio_reader/map/google_map/google_map_source.js
+++ biblio_reader/map/google_map/google_map_source.js
@@ -1,16 +1,15 @@
 function initMap() {
-        var uluru = {lat: -25.363, lng: 131.044};
         var map = new google.maps.Map(document.getElementById('map'), {
           zoom: 2,
           center: {lat: 0, lng: 0}
         });
-        var marker = new google.maps.Marker({
-          position: uluru,
+        $.getJSON("affiliations.json", function(affils) {
+          $.each(affils, function(geo_key, attrs) {
+          var latlong = geo_key.split(',')
+          var marker = new google.maps.Marker({
+          position: {lat: parseFloat(latlong[0]), lng: parseFloat(latlong[1])},
           map: map
           })
-        var marker = new google.maps.Marker({
-          position: {lat: 42.3631542, lng:-71.0688334},
-          map: map
           })
-        $getJSON()
+        })
 }
