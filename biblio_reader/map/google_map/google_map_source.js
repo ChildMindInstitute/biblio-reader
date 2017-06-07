e45905f0896c6864ddce702b4a97282120f8bf6e
mfleisch50 <mfleisch50@gmail.com>
2017-06-07 15:12:52 -0400

    New geo json affil file

--- biblio_reader/map/google_map/google_map_source.js
+++ biblio_reader/map/google_map/google_map_source.js
@@ -1,9 +1,16 @@
 function initMap() {
         var uluru = {lat: -25.363, lng: 131.044};
         var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 4,
-          center: uluru
+          zoom: 2,
+          center: {lat: 0, lng: 0}
         });
         var marker = new google.maps.Marker({
           position: uluru,
           map: map
+          })
+        var marker = new google.maps.Marker({
+          position: {lat: 42.3631542, lng:-71.0688334},
+          map: map
+          })
+        $getJSON()
+}
