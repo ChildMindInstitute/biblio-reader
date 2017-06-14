d08f38e8c0a484df68413e92d5c02057661bef8c
mfleisch50 <mfleisch50@gmail.com>
2017-06-14 16:46:27 -0400

    Updated map and table data

--- table/data_mg.py
+++ table/data_mg.py
@@ -1,6 +1,6 @@
 import manager as mg
 
 data = mg.get_data()
-data = data[data['Data Use'] == 'Y'][data['Journal Category'] == 'Proceeding']
+data = data[data['Data Use'] == 'Y'][data['Journal Category'] == 'Thesis']
 table_data = data[['Title', 'Authors', 'Year', 'URL']].sort_values(['Year', 'Title'])
 table_data.to_csv(path_or_buf='Table_data.csv', index=False)
