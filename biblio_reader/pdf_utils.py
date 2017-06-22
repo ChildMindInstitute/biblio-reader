c8233ef27c781924ecda306131dea589c6149872
mfleisch50 <mfleisch50@gmail.com>
2017-06-22 18:29:13 -0400

    PDF Utils update

--- biblio_reader/pdf_utils.py
+++ biblio_reader/pdf_utils.py
@@ -5,27 +5,30 @@ data = mg.get_data()
 
 def pdfopener(data, dir):
     """
-
-    :param data:
-    :param dir:
-    :return:
+    Opens all PDF URLs in the dataframe
     """
-    for row in data.iterrows():
+    for i, row in data.iterrows():
         url = row[1]['URL']
         if pd.isnull(url):
             continue
         if '.pdf' in url:
             try:
                 pdf = urllib.urlopen(url)
-                with open(dir + str(row[1]['i']) + '.pdf', 'wb') as f:
+                with open(dir + str(i) + '.pdf', 'wb') as f:
                     f.write(pdf.read())
             except:
-                print('Unable to open:', row[1]['i'])
+                print('Unable to open:', i)
                 print(url)
                 continue
 
 
-def pdffinder(data):
+def pdffinder(data, dir):
+    """
+    Tries to find as many PDFs as possible from URLs in the dataframe
+    :param data: A zip list of article reference #s and their URLS
+    :param dir: Out directory
+    """
+    data = dict(data)
     pdfs_found = 0
     for key in data:
         link = data[key]
@@ -49,7 +52,7 @@ def pdffinder(data):
                     pdf = domain + pdf
                 try:
                     pdf_file = urllib.urlopen(pdf).read()
-                    with open('../linked_pdfs/' + str(key) + '.pdf', 'wb') as f:
+                    with open(os.path.join(dir, str(key)) + '.pdf', 'wb') as f:
                         f.write(pdf_file)
                     pdfs_found += 1
                     break
@@ -58,7 +61,12 @@ def pdffinder(data):
     print('PDFS FOUND: ' + str(pdfs_found))
 
 
-def arxiv_open(data):
+def arxiv_open(data, dir):
+    """
+    Writes all PDFS found on arXiv to the directory
+    :param data: A zip list of article reference #s and their URLS
+    """
+    data = dict(data)
     for key in data:
         link = data[key]
         try:
@@ -74,13 +82,18 @@ def arxiv_open(data):
                 pdf = 'https://arxiv.org' + url.get('href') + '.pdf'
                 try:
                     pdf_file = urllib.urlopen(pdf).read()
-                    with open('../linked_pdfs/' + str(key) + '.pdf', 'wb') as f:
+                    with open(os.path.join(dir, str(key)) + '.pdf', 'wb') as f:
                         f.write(pdf_file)
                     break
                 except Exception as e:
                     print(e, key, pdf)
 
-def plos_open(data):
+def plos_open(data, dir):
+    """
+    Writes all PDFS found on PLoS website to the dir
+    :param data: A zip list of article reference #s and their URLS
+    """
+    data = dict(data)
     for key in data:
         link = data[key]
         try:
@@ -96,7 +109,7 @@ def plos_open(data):
                 pdf = 'http://journals.plos.org' + url.get('href')
                 try:
                     pdf_file = urllib.urlopen(pdf).read()
-                    with open('../linked_pdfs/' + str(key) + '.pdf', 'wb') as f:
+                    with open(os.path.join(dir, str(key)) + '.pdf', 'wb') as f:
                         f.write(pdf_file)
                     break
                 except Exception as e:
@@ -104,6 +117,11 @@ def plos_open(data):
 
 
 def liebert_open(data):
+    """
+    Writes all PDFS found on Liebert website to the directory
+    :param data: A zip list of article reference #s and their URLS
+    """
+    data = dict(data)
     for key in data:
         link = data[key]
         try:
@@ -119,40 +137,34 @@ def liebert_open(data):
                 pdf = url.get('href')
                 try:
                     pdf_file = urllib.urlopen(pdf).read()
-                    with open('../linked_pdfs/' + str(key) + '.pdf', 'wb') as f:
+                    with open(os.path.join(dir, str(key)) + '.pdf', 'wb') as f:
                         f.write(pdf_file)
                     break
                 except Exception as e:
                     print(e, key, pdf)
 
 
-def frontiers_open(data):
-    for key in data:
-        link = data[key]
-        pdf = link.replace('full', 'pdf').replace('abstract', 'pdf')
-        if 'pdf' not in pdf:
-            pdf = pdf + '/pdf'
-        print(pdf)
-        try:
-            pdf_file = urllib.urlopen(pdf).read()
-            with open('../linked_pdfs/' + str(key) + '.pdf', 'wb') as f:
-                f.write(pdf_file)
-        except Exception as e:
-            print(e, key, pdf)
-
-
-def citeseer_open(data):
+def citeseer_open(data, dir):
+    """
+    Writes all PDFs found on the Citeseerx website to the directory
+    :param data: A zip list of article reference #s and their URLS
+    """
+    data = dict(data)
     for key in data:
         link = data[key]
         try:
             pdf_file = urllib.urlopen(link).read()
-            with open('../linked_pdfs/' + str(key) + '.pdf', 'wb') as f:
+            with open(os.path.join(dir, str(key)) + '.pdf', 'wb') as f:
                 f.write(pdf_file)
         except Exception as e:
             print(e, key, link)
 
 
 def find_corrupted(pdf_directory):
+    """
+    Finds all PDFs in the PDF directory that cannot be opened
+    :return: List of file names that cannot be opened
+    """
     res = []
     for path, dirs, files in os.walk(pdf_directory):
         for file in files:
@@ -163,37 +175,17 @@ def find_corrupted(pdf_directory):
                 res.append(int(file.replace('.pdf', '')))
     return res
 
-"""
-print([i for i, paragraph in paragraphs.items() if len(paragraph) == 0])
-print([i for i in range(len(data)) if i not in paragraphs])
-print([i for i, sets in sets.items() if len(sets) == 0])
-#mg.update_data()
-""""""
-data['Sets'] = assoc_sets(TXT_DIR, mg.WEIGHTED_SETS, less_weighted_sets=mg.UNWEIGHTED_SETS).values()
-mg.update_data()
 
-def main():
+def find_articles_left(data, dir):
+    """
+    Finds and prints a list of all articles that do not have PDFs yet
+    :param data: A zip list of article reference numbers and their URLS
+    :param dir: The PDF directory
+    """
+    unlinkables = []
+    for i, url in data:
+        if i not in [file.replace('.txt', '') for file in os.listdir(dir)]:
+            print(i, url)
+            unlinkables.append((i, url))
+    return unlinkables
 
-    PDF_DIR = mg.dir(os.path.join(mg.INPUT_PATH, 'pdfs'))
-
-    TXT_DIR = mg.dir(os.path.join(mg.ROOT_PATH, 'txts'))
-
-    dict_data = dict(zip(data['i'], data['URL']))
-
-
-    if len(os.listdir(PDF_DIR)) == 0:
-        pass
-
-    dict_data = dict(zip(data['i'], data['URL']))
-    dict_titles = dict(zip(data['i'], zip(data['Title'], data['URL'])))
-    valid_data = {key: value for key, value in dict_data.items() if not isinstance(value, float)}
-    pdfs = [int(pdf.replace('.pdf', '')) for pdf in os.listdir('../inputs/pdfs') if not pdf.startswith('.')]
-    no_pdfs = {key: value for key, value in dict_data.items() if key not in pdfs}
-    print(len(pdfs))
-    convertToText.walkAndText(mg.dir(os.path.join(mg.INPUT_PATH, 'pdfs')), '../outputs/txts')
-    paragraph_dict = find_paragraphs('../outputs/txts', mg.FCP_TERMS, outfile='../outputs/paragraphs.txt')
-    empty_paragraphs = [key for key, value in paragraph_dict.items() if len(value) == 0]
-    bad_data = list(no_pdfs) + empty_paragraphs
-    data[data['i'].isin(bad_data)].to_csv(path_or_buf='../outputs/unlinkables.csv', index=False)
-
-"""
