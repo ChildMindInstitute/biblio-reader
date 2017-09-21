ee9fdc28cc4e44d23ddfbb8d4cee125e2b443d30
Jon Clucas <jon.clucas@childmind.org>
2017-09-21 09:21:50 -0400

    import manager

--- biblio_reader/authors_to_json.py
+++ biblio_reader/authors_to_json.py
@@ -1,16 +1,19 @@
-import manager as mg, json, os
+import os
+import sys
+import json
+br_path = os.path.abspath(os.path.join(__file__,
+                          os.pardir, os.pardir))
+if br_path not in sys.path:
+    sys.path.append(br_path)
+import manager as mg
 data = mg.get_data()
-data = data[data['Data Use'] == 'Y'].dropna(subset=['Authors'])
+data = data[(data['Data Use'] == 'Y')].dropna(subset=['Authors'])
 from biblio_reader import scholar_reader
 
 
 types = {auth: 'Contributor' in val for auth, val in scholar_reader.authors(data, 'Contributor').items()}
 contributors = {author for i, authors in zip(data['i'], data['Authors']) for author in authors.split(' & ')
                             if i in mg.CONTR_PAPERS}
-
-
-
-
 
 
 with open(os.path.join(mg.dir(os.path.join('data', 'author-links')), 'objects.json'), 'w') as o:
