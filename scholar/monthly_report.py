4a648a7759325516949093ec4866913f6be1cfc2
Jon Clucas <jon.clucas@childmind.org>
2017-09-29 14:48:15 -0400

    year only

---
+++ scholar/monthly_report.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+monthly_report.py
+
+Created on Thu Sep 21 09:28:06 2017
+
+@author: jon.clucas
+"""
+from datetime import date, timedelta
+import subprocess
+
+global keywords
+keywords = [
+               "fcon_1000.projects.nitrc.org",
+               "Rockland Sample",
+               "1000 Functional Connectomes",
+               "International Neuroimaging Data-Sharing Initiative",
+               "Autism Brain Imaging Data Exchange",
+               "ADHD-200",
+               "Consortium for Reproducibility and Reliability"
+           ]
+
+command = "python biblio_reader.py -s \"{0}\" --before {1} --after {2} -o \""\
+          "{3}\"".format(
+              ", ".join([k for k in keywords]),
+              date(date.today().year),
+              date(date.today().year))
+          )
+print(command)
+subprocess.call(command, shell=True)
