64e8a4494154752cea17fb5166292a9c4e4ec938
Jon Clucas <jon.clucas@childmind.org>
2017-10-11 18:45:12 -0400

    get some pdfs, get links for rest 😅

--- biblio_reader/pdf_utils.py
+++ biblio_reader/pdf_utils.py
@@ -2,6 +2,7 @@ from bs4 import BeautifulSoup as bs
 import os
 import pandas as pd
 import PyPDF2
+from pdfrw import PdfReader
 import sys
 import urllib.parse as urlparse
 import urllib.request as urllib
@@ -9,8 +10,64 @@ ancestor = os.path.abspath(os.path.join(os.pardir, os.pardir))
 if ancestor not in sys.path:
     sys.path.append(ancestor)
 from Biblio_Reader import manager as mg
+from numpy import nan
 
 
+def check_url(s):
+    """
+    Function to check for existance and 'http://scholar.google.com/' prefix
+    
+    Parameters
+    ----------
+    s : string
+    
+    Returns
+    -------
+    s : string or None
+    """
+    if ((s in {"", nan, "nan", None}) or
+        (len(s) <= 3)):
+        return(None)
+    elif s.startswith(
+        'http://scholar.google.com/'):
+        return(s[26:])
+    else:
+        return(s)
+        
+
+def pdf_anchor_finder(url):
+    """
+    Function to find pdf links on a given page
+    
+    Parameters
+    ----------
+    url : string
+    
+    Returns
+    -------
+    pdf_urls : list of strings
+    """
+    try:
+        html = urllib.urlopen(url).read()
+    except:
+        return([])
+    if isinstance(url, str):
+        domain = urlparse.urljoin(url, '/')[:-1]
+    else:
+        return([])
+    soup = bs(html, 'html.parser')
+    pdf_urls = [url] if 'pdf' in url else []
+    for url_a in soup.find_all('a'):
+        if url_a.get('href') is None:
+            return([])
+        if 'pdf' in url_a.get('href'):
+            pdf = url_a.get('href')
+            if pdf.startswith('/'):
+                pdf = domain + pdf
+            pdf_urls.append(pdf)
+    return(pdf_urls)
+
+        
 def pdfopener(data, dir):
     """
     Opens all PDF URLs in the dataframe
@@ -41,37 +98,73 @@ def pdffinder(data, dir):
     data = dict(data)
     pdfs_found = 0
     for key in data:
-        link = data[key]
-        link = link[26:] if link.startswith(
-                'http://scholar.google.com/') else link
-        try:
-            html = urllib.urlopen(link).read()
-        except:
-            print(key, link)
-            continue
-        if isinstance(link, str):
-            domain = urlparse.urljoin(link, '/')[:-1]
-        else:
-            print('No domain name found:', key, type(link))
-            continue
-        soup = bs(html, 'html.parser')
-        for url in soup.find_all('a'):
-            if url.get('href') is None:
-                continue
-            if '.pdf' in url.get('href'):
-                pdf = url.get('href')
-                if pdf.startswith('/'):
-                    pdf = domain + pdf
-                try:
-                    pdf_file = urllib.urlopen(pdf).read()
-                    with open(os.path.join(dir, str(key)) + '.pdf', 'wb') as f:
-                        f.write(pdf_file)
-                    pdfs_found += 1
-                    break
-                except Exception as e:
-                    print(e, key, pdf)
+        link = check_url(data[key])
+        pdfs = pdf_anchor_finder(link)
+        for pdf in pdfs:
+            try:
+                pdf_file = urllib.urlopen(pdf).read()
+                fp = os.path.join(dir, str(key)) + '.pdf'
+                fp_i = 1
+                while(os.path.exists(fp)):
+                    fp = os.path.join(dir, "{0}_{1}.pdf".format(str(key), fp_i))
+                    fp_i = fp_i + 1
+                with open(fp, 'wb') as f:
+                    f.write(pdf_file)
+                pdfs_found += 1
+                break
+            except Exception as e:
+                print(e, key, pdf)
     print('PDFS FOUND: ' + str(pdfs_found))
+    
 
+def pdf_finder(data, dir):
+    """
+    Tries to find as many PDFs as possible from URLs in the dataframe
+    :param data: DataFrame
+    :param dir: Out directory
+    """
+    pdf_links = dict(zip(data['i'], data['PDF link']))
+    urls = dict(zip(data['i'], data['URL']))
+    pdfs_found = 0
+    for key in urls:
+        fp = os.path.join(dir, str(key)) + '.pdf'
+        tell_me = "\033[1m{0}:\033[0m".format(key)
+        if os.path.exists(fp):
+            tell_me = "{0}\n{1} exists".format(tell_me, fp)
+        else:
+            link = check_url(pdf_links[key])
+            link = link if link else check_url(urls[key])
+            if link:
+                tell_me = "{0}\noriginal link: {1}".format(tell_me, link)
+                pdfs = pdf_anchor_finder(link)
+                if len(pdfs):
+                    for pdf in pdfs:
+                        try:
+                            pdf_file = urllib.urlopen(pdf).read()
+                            fp_i = 1
+                            while(os.path.exists(fp)):
+                                fp = os.path.join(dir, "{0}_{1}.pdf".format(str(key), fp_i))
+                                fp_i = fp_i + 1
+                            with open(fp, 'wb') as f:
+                                f.write(pdf_file)
+                            try:
+                                x = PdfReader(pdf_file)
+                                if(len(x.pages) > 1):
+                                    tell_me = "{0}\n{1} saved as {2}".format(tell_me, pdf, fp)
+                                    pdfs_found += 1
+                                else:
+                                    tell_me = "{0}\n{1} only one page, not saved".format(tell_me, pdf)
+                                    os.remove(pdf_file)
+                            except:
+                                tell_me = "{0}\n{1} failed to save as valid pdf".format(tell_me, pdf)
+                                os.remove(pdf_file)
+                        except Exception as e:
+                            tell_me = "{0}\n{1}: {2} {3}".format(tell_me, e, pdf, link)
+                            continue
+            else:
+                tell_me = "{0}\n{1}".format(tell_me, "no URL")
+        print(tell_me)
+        
 
 def arxiv_open(data, dir):
     """
